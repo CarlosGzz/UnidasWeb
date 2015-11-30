9ae896a26c83c1b7ec6c4661c9c66064136c659c
CarlosGzz <carlosgzz93@gmail.com>
2015-11-29 22:22:08 -0600

    Update connect.php

--- unidasweb2/Modelo/connect.php
+++ unidasweb2/Modelo/connect.php
@@ -1,15 +1,13 @@
 <?php
-	define('HOST','fdb7.biz.nf');
-  define('USER','1982557_unidasbd');
-  define('PASS','unidasbd1');
-  define('DB','1982557_unidasbd');
-  $con = mysqli_connect(HOST,USER,PASS,DB);
-        
-	
+	$servername = "us-cdbr-iron-east-03.cleardb.net";
+	$username = "beb3bc414324dd";
+	$password = "97741eec";
+	$dbname = "heroku_0da25f9d8cd62f7";
 	// Create connection
-	$db = new mysqli(HOST,USER,PASS,DB);
+	$db = new mysqli($servername, $username, $password, $dbname);
 	// Check connection
 	if ($db->connect_error) {
 		die("Connección fallida: Lo sentimos estamos teniendo problemas" . $db->connect_error);
 	}
+	
 ?>
